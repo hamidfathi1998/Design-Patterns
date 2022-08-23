52abdc9f0a30fe894732ed69f4977f7d7e1bbe6e
Hamid Fathi <hamid.fathi.developer@gmail.com>
2022-08-23 12:04:46 +0430

    add Singleton

--- app/src/main/java/ir/hfathi/designpattern/MainActivity.kt
+++ app/src/main/java/ir/hfathi/designpattern/MainActivity.kt
@@ -2,14 +2,23 @@ package ir.hfathi.designpattern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import ir.hfathi.designpattern.behavioralPatterns.singletion.Singleton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        setUpSingleton()
     }
 
+    private fun setUpSingleton() {
+        val first = Singleton.instance  // This (Singleton@7daf6ecc) is a
+        // singleton
+        first.b = "hello singleton"
+
+        val second = Singleton.instance
+        println(first.b)        // hello singleton
+        println(second.b)        // hello singleton
+    }
 }
---
+++ app/src/main/java/ir/hfathi/designpattern/behavioralPatterns/singletion/Singleton.kt
@@ -0,0 +1,12 @@
+package ir.hfathi.designpattern.behavioralPatterns.singletion
+
+public class Singleton private constructor() {
+    init { println("This ($this) is a singleton") }
+
+    private object Holder { val INSTANCE = Singleton() }
+
+    companion object {
+        val instance: Singleton by lazy { Holder.INSTANCE }
+    }
+    var b:String? = null
+}
